ab4c2750273d086747081323da7aa56033bfff52
Kasemt2411 <163806144+Kasemt2411@users.noreply.github.com>
2024-03-17 23:03:58 -0500

    Add files via upload

---
+++ apiEjemplo/apiEjemplo/src/test/java/com/ejemploapi/ApiEjemploApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejemploapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEjemploApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
